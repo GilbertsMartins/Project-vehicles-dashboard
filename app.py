fbb9c93e45c9fdb5754f40ddbd33a2eede096a08
GilbertsMartins <gilbertgsmartins@gmail.com>
2025-11-26 22:49:36 +0000

    Update beta

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ car_data['model_year'] = car_data['model_year'].astype(int)
 
 # cabeçalho do aplicativo
 st.header('A dataset view of used vehicles for sale') 
+st.write('This is a way to filter vehicles by the price you choose.')
+# Filter vehicles by price using this button
+build_table = st.checkbox('I agree') # checkbox para começar as filtragens.
 
-build_table = st.checkbox('Filter vehicles by price') # Checkbox para criar uma tabela filtrada com o valor dos veículos
-
-if build_table:
+if build_table: # Se o botão for clicado
     by_price_min = st.number_input('Insert the min value of the vehicle',
                                value=None, placeholder='Type the price... $USD')
     by_price_max = st.number_input('Insert the max value of the vehicle',
@@ -42,31 +43,32 @@ if build_table:
         st.write('Model: ', hist_option_model) # escrever o modelo selecionado
         # criar um histograma
         if hist_option_model:
-            st.write('The histogram above shows the correlation between vehicle price and mileage for the user-filtered vehicles.')
+            st.write('The histogram shows the distribution of vehicle prices, with colors representing different model years.')
             filtered_cars = filter_by_price[(filter_by_price['manufacture'] == hist_option_brand) & 
                                     (filter_by_price['model'] == hist_option_model)]
-            fig = px.histogram(filtered_cars, x='price', y='odometer', 
-                               title='Correlation between vehicle price and mileage', 
-                               color='model_year',
+            filtered_cars['year_range'] = pd.cut(filtered_cars['model_year'], 
+                                   bins=[1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020], 
+                                   labels=['1941-1950', '1951-1960', '1961-1970', '1971-1980', '1981-1990', 
+                                           '1991-2000', '2001-2010', '2011-2020'])
+            fig = px.histogram(filtered_cars, x='price', 
+                               title=(f'Distribution of Prices - {hist_option_brand} | {hist_option_model}'), 
+                               color='year_range',
                                 )
-        # exibir um gráfico Plotly interativo
-        st.plotly_chart(fig, width='stretch')
+            # exibir um gráfico Plotly interativo
+            st.plotly_chart(fig, width='stretch')
 
-    # criar um botão para o gráfico de dispersão
-    chart_button = st.button('Criar gráfico de dispersão') 
-
-    if chart_button: # se o botão for clicado
-        # escrever uma mensagem
-        st.write('The chart above displays data from the advertisements, showing the cars’ ' \
-        'odometer readings and model years. The odometer values are reported in miles.')
-        # criar um gráfico de dispersão
-        fig = px.scatter(filtered_cars, x='odometer', y='model_year')
-        # exibir um gráfico Ploty interativo
-        st.plotly_chart(fig, width='stretch')
+        if build_table:
+            # escrever uma mensagem
+            st.write('The chart above displays data from the advertisements, showing the cars’ ' \
+            'odometer readings and model years. The odometer values are reported in miles.')
+            # criar um gráfico de dispersão
+            fig = px.scatter(filtered_cars, x='odometer', y='model_year')
+            # exibir um gráfico Ploty interativo
+            st.plotly_chart(fig, width='stretch')
 
 
-# criar um gráfico de barras
-fig = px.bar(car_data, x='manufacture', color='type', title='Vehicles types by manufacture')
-# exibir um gráfico plotly interativo
-st.plotly_chart(fig, width='stretch')
+            # criar um gráfico de barras
+            fig = px.bar(filter_by_price, x='manufacture', color='type', title='Vehicles types by manufacture')
+            # exibir um gráfico plotly interativo
+            st.plotly_chart(fig, width='stretch')
 
